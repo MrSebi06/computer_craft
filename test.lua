62665527cdc8f3986660d0b384c71bbb5703edea
MrSebi06 <mihneasebastian06@gmail.com>
2025-01-10 23:54:27 +0100

    Add prints

--- test.lua
+++ test.lua
@@ -3,6 +3,9 @@ Number = math.random(1, 100)
 ---@type integer
 Guess = nil
 
+print("### Guess the number ###")
+print("A number has been chosen between 1 and 100. Guess it!")
+
 while Guess ~= Number do
     Guess = tonumber(io.read())
     if Guess < Number then
